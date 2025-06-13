e1edbf996b36ac24c2dd2d3e64a68e29097a1d89
Jacques Mosseri <jacquesimosseri@gmail.com>
2025-06-13 12:57:18 -0400

    testing font change4

--- GetNYY1.py
+++ GetNYY1.py
@@ -1,28 +1,14 @@
+from matplotlib.font_manager import FontProperties
 import matplotlib.pyplot as plt
-import matplotlib.font_manager
 
-# (If you added the font to the Matplotlib font directory)
-# Delete the font cache to rebuild it
-# import shutil
-# cache_dir = matplotlib.get_cachedir()
-# if cache_dir:
-#    shutil.rmtree(cache_dir, ignore_errors=True)
+georgia_font = FontProperties(fname='/usr/share/fonts/truetype/msttcorefonts/georgia.tff') # Replace with the actual path
 
-# (If you want to use the font by directly referencing the file path)
-# from matplotlib.font_manager import FontProperties
-# georgia_font = FontProperties(fname='/path/to/georgia.ttf')
+x = [1, 2, 3]
+y = [4, 5, 6]
 
-# Set Georgia as the default font family
-plt.rcParams['font.family'] = 'Georgia'
-
-# Create a sample plot
-x = [1, 2, 3, 4, 5]
-y = [2, 4, 6, 8, 10]
 plt.plot(x, y)
-
-# Add title and labels
-plt.title('Plot with Georgia Font')
-plt.xlabel('X-axis Label')
-plt.ylabel('Y-axis Label')
+plt.xlabel('X-axis', fontproperties=georgia_font)
+plt.ylabel('Y-axis', fontproperties=georgia_font)
+plt.title('Plot with Georgia Font', fontproperties=georgia_font)
 
 plt.show()
